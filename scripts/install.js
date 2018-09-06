4df7dd70c7cdc063a071aa7fcfee01c33169d99b
minus78 <minus78@users.noreply.github.com>
2018-09-06 21:43:46 +0200

    Modify install.js to work in Windows
    process.env.PWD doesn't exist in Windows, I replace by process.cwd()

--- scripts/install.js
+++ scripts/install.js
@@ -2,23 +2,47 @@ const path = require('path');
 const fs = require('fs');
 const fse = require('fs-extra');
 
-if (process.env.PWD.match(/node_modules/)) {
-	// move sources
-	fse.removeSync('js');
-	fse.moveSync('dist/js','js');
-	fse.removeSync('sass');
-	fse.moveSync('src/sass','sass');
+if (process.platform === "win32") {
+	if (process.cwd().match(/node_modules/)) {
+		// move sources
+		fse.removeSync('js');
+		fse.moveSync('dist/js','js');
+		fse.removeSync('sass');
+		fse.moveSync('src/sass','sass');
 
-	// clean repo
-	fse.removeSync('src');
-	fse.removeSync('dist');
-	fse.removeSync('.sass-cache');
-	fse.removeSync('favicon.ico');
-	fse.removeSync('.DS_Store');
-	fse.removeSync('scripts');
-	fse.removeSync('demo');
+		// clean repo
+		fse.removeSync('src');
+		fse.removeSync('dist');
+		fse.removeSync('.sass-cache');
+		fse.removeSync('favicon.ico');
+		fse.removeSync('.DS_Store');
+		fse.removeSync('scripts');
+		fse.removeSync('demo');
 
-	// update sass files at root
-	fs.writeFileSync('_index.scss', '@import "sass/gridle";');
-	fs.writeFileSync('_flex.scss', '@import "sass/gridle-flex";');
+		// update sass files at root
+		fs.writeFileSync('_index.scss', '@import "sass/gridle";');
+		fs.writeFileSync('_flex.scss', '@import "sass/gridle-flex";');
+	}
+} else {
+	
+	if (process.env.PWD.match(/node_modules/)) {
+		// move sources
+		fse.removeSync('js');
+		fse.moveSync('dist/js','js');
+		fse.removeSync('sass');
+		fse.moveSync('src/sass','sass');
+
+		// clean repo
+		fse.removeSync('src');
+		fse.removeSync('dist');
+		fse.removeSync('.sass-cache');
+		fse.removeSync('favicon.ico');
+		fse.removeSync('.DS_Store');
+		fse.removeSync('scripts');
+		fse.removeSync('demo');
+
+		// update sass files at root
+		fs.writeFileSync('_index.scss', '@import "sass/gridle";');
+		fs.writeFileSync('_flex.scss', '@import "sass/gridle-flex";');
+	}	
 }
